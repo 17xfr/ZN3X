e71440ca19e797ca867f325de83a745a159fddac
Fusingsign254111 <92048485+Fusingsign254111@users.noreply.github.com>
2025-09-02 19:10:39 +0100

    goon

--- fix loadstring.lua
+++ fix loadstring.lua
@@ -8,3 +8,12 @@ end
 game.Players.PlayerAdded:Connect(function(Player)
 	table.insert(Skeletons, Library:NewSkeleton(Player, true));
 end)
+---{local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/ESPs/main/UniversalSkeleton.lua"))()
+
+--local Skeletons = {}
+--for _, Player in next, game.Players:GetChildren() do
+--	table.insert(Skeletons, Library:NewSkeleton(Player, true));
+--end
+--game.Players.PlayerAdded:Connect(function(Player)
+--	table.insert(Skeletons, Library:NewSkeleton(Player, true));
+--end)
